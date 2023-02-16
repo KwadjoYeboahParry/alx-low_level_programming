52ffa4a6d708aa6dc23d14f92d648296632ee244
KwadjoYeboahParry <parry26yeboah@gmail.com>
2023-02-16 22:00:52 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using printf to output a line;
+ * Return: 0 (Successfully run)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
