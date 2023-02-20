f3e3b7978485f3348cebb34d3172bda3e04a5f3f
KwadjoYeboahParry <parry26yeboah@gmail.com>
2023-02-20 18:42:11 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - prints base ten single digits with putchar only
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
