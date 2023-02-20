86427248fe480848cc0250c5f93c8eecc7db7ed0
KwadjoYeboahParry <parry26yeboah@gmail.com>
2023-02-20 20:31:15 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,19 @@
  */
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 48; n < 58; n++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
 
-	return (0);
+	putchar('\n')
+
+		return (0);
 }
