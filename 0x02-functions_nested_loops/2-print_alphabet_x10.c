6b51941801e57661c3614c8c7d0fbd9490ebc91c
KwadjoYeboahParry <parry26yeboah@gmail.com>
2023-02-21 18:04:45 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - prints 10 times the alphabets in lowercase
+ * followed by a new line
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i;
+
+	while (i <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
