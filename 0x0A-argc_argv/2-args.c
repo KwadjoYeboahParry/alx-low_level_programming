9df0249e34342e5ee6538f09fa22110024926e58
KwadjoYeboahParry <parry26yeboah@gmail.com>
2023-04-07 14:46:02 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: (0)
  */
 
 int main(int argc, char *argv[])
